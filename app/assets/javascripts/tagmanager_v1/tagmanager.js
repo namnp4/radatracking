e706a9fce7c2a8d0ca4e139b1116d0f8a7617617
DESKTOP-EFC7D2N\TOPICAN <namnp4@topica.edu.vn>
2018-10-22 19:21:37 +0700

    fix tagmanager.js

--- app/assets/javascripts/tagmanager_v1/tagmanager.js
+++ app/assets/javascripts/tagmanager_v1/tagmanager.js
@@ -1,11 +1,11 @@
 $(document).ready(function(){
 
-  function tracking_spy(){
-    $("#tracking_spy").click(function(){
-      Spymaster.track("video","play","abc");
-    });
-  }
-  tracking_spy();
+  // function tracking_spy(){
+  //   $("#tracking_spy").click(function(){
+  //     Spymaster.track("video","play","abc");
+  //   });
+  // }
+  // tracking_spy();
   var obj = {
     name: "",
     selector: {},
